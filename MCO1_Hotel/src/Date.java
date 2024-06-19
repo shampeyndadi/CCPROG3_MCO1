df7614de46a6825def0d831567642d77b3241d38
Jedidiah <152191672+shampeyndadi@users.noreply.github.com>
2024-06-20 01:54:19 +0800

    Update Date.java

--- MCO1_Hotel/src/Date.java
+++ MCO1_Hotel/src/Date.java
@@ -1,24 +1,17 @@
-import java.util.ArrayList; 
-import java.util.Arrays;
-
 public class Date {
     
-    private int  month ;
-    private int year ;
+    private String month;
+    private int year;
     private int  day;
     
-    public Date(int month, int year, int day) {
+    public Date(String month, int year, int day) {
         this.month = month;
         this.year = year;
         this.day = day;
     }
 
     public String getDate(){
-        ArrayList<String> months = new ArrayList<>(Arrays.asList("January", "February", "March", "April",
-                                                               "May", "June", "July", "August", "September", 
-                                                                "October", "November", "December"));
-
-        return months.get(month - 1) + " | " + this.day +  " | " + this.year; 
+        return month + " " + day + " " + year;
     }
 
 }
