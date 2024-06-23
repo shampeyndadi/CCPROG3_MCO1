f6ce71c35f68035dd0f47600860d8400de88e98c
Jedidiah <152191672+shampeyndadi@users.noreply.github.com>
2024-06-23 22:36:48 +0800

    Add files via upload

---
+++ MCO1_Hotel/src/Execute.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+
+public class Execute{
+    private Hotels HRS;
+    private ManageHotel HotelManager;
+    private SimulateBooking bookHotel;
+    private Scanner sc;
+    private Interface layout;
+    
+
+    public Execute(){
+        this.HRS = new Hotels();
+        this.HotelManager = new ManageHotel(HRS);
+        this.bookHotel = new SimulateBooking(HRS);
+        this.sc = new Scanner(System.in);
+        this.layout = new Interface();
+    }
+
+    public void clearConsole(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (Exception e) {
+            for (int i = 0; i < 50; ++i) 
+                System.out.println();
+        } 
+    }
+
+    public void pressEnterToContinue()
+    { 
+           System.out.println("Press Enter key to continue...");
+           try
+           {
+               System.in.read();
+           }  
+           catch(Exception e)
+           {}  
+    }
+
+    public void ManageHotel(){
+        boolean run = true;
+        int option;
+    
+        do{
+            layout.ManageHotel();
+            System.out.print("\n          [Enter option]: ");
+            option = sc.nextInt();
+            switch(option){
+                case 1:
+                    clearConsole();
+                    HotelManager.AddRooms();
+                    pressEnterToContinue();
+                    clearConsole();
+                    break;
+                case 2:
+                    clearConsole();
+                    HotelManager.changeHotelName();
+                    pressEnterToContinue();
+                    clearConsole();
+                    break;
+                case 5:
+                    clearConsole();
+                    HotelManager.removeHotel();
+                    pressEnterToContinue();
+                    clearConsole();
+                case 6: 
+                    clearConsole();
+                    run = false;
+                    clearConsole();
+                    break; 
+            }
+        } while (run != false);
+    }
+
+    public void execute(){
+        boolean run = true;
+        int option;
+        
+        do{
+            layout.MainMenu();
+            System.out.print("\n          [Enter option]: ");
+            option = sc.nextInt();
+            switch(option){
+                case 1:
+                    clearConsole();
+                    HRS.createHotel();
+                    pressEnterToContinue();
+                    clearConsole();
+                    break;
+                case 2:
+                    clearConsole();
+                    ManageHotel();
+                    break;
+                case 3: 
+                    clearConsole();
+                    bookHotel.bookHotel();
+                    break;
+                case 5: 
+                    run = false;
+                    clearConsole();
+                    System.out.println("[Program End]");
+                    break; 
+            }
+        } while (run != false);
+        
+    }
+}   
---
+++ MCO1_Hotel/src/SimulateBooking.java
@@ -0,0 +1,174 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SimulateBooking {
+    private Hotels HotelLists;
+    private Scanner sc;
+
+    public SimulateBooking(Hotels HotelLists){
+        this.HotelLists = HotelLists;
+        this.sc = new Scanner(System.in);
+    }
+
+    public void bookHotel(){
+        if (HotelLists.listOfHotels().isEmpty()){
+            System.out.println("\nNo hotels yet! Hotels are yet to be built");
+        }else{
+            Execute exe = new Execute();
+            HotelLists.DisplayHotels();
+
+            System.out.print("\nChoose hotel [Enter index]: ");
+            int index = sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("\nEnter preferred check in date: ");
+            System.out.print("*------------------------------------*\n\n");
+            System.out.print("Enter month: ");
+            String checkInMonth = sc.nextLine();
+    
+            while (!verifyMonth(checkInMonth)){
+                System.out.print("Error invalid month! Enter month: ");
+                checkInMonth = sc.nextLine();
+            }
+
+            System.out.println();
+            
+            System.out.print("Enter day: ");
+            int checkInDay = sc.nextInt();
+            sc.nextLine();
+
+            while(!(checkInDay < 31)){
+                System.out.print("Error invalid day! Enter day: ");
+                checkInDay = sc.nextInt();
+            }
+
+            System.out.println();
+
+            System.out.print("Enter year: ");
+            int checkInYear = sc.nextInt();
+
+            while (!(checkInYear <= 2024)){
+                System.out.print("Error enter valid year! Enter year: ");
+                checkInYear = sc.nextInt();    
+            }
+
+            sc.nextLine();
+            System.out.println();
+
+            exe.clearConsole();
+
+            System.out.println("\nEnter preferred check out date: ");
+            System.out.print("*------------------------------------*\n\n");
+            System.out.print("Enter month: ");
+            String checkOutMonth = sc.nextLine();
+
+            while (!verifyMonth(checkOutMonth)){
+                System.out.print("Error invalid month! Enter month: ");
+                checkOutMonth = sc.nextLine();
+            }
+
+            System.out.println();
+
+            System.out.print("Enter day: ");
+            int checkOutDay = sc.nextInt();
+
+            while(!(checkOutDay < 31)){
+                System.out.print("Error invalid day! Enter day: ");
+                checkOutDay = sc.nextInt();
+            }
+
+            System.out.println();
+
+            System.out.print("Enter year: ");
+            int checkOutYear = sc.nextInt();
+
+            while (!(checkOutYear <= 2024)){
+                System.out.print("Error enter valid year! Enter year: ");
+                checkOutYear = sc.nextInt();    
+            }
+
+            System.out.println();
+
+            Date checkIn = new Date(checkInMonth, checkInYear, checkInDay);
+            Date checkOut = new Date(checkOutMonth, checkOutYear, checkOutDay);
+
+            if (checkIn.after(checkOut)){
+                System.out.println("Error: Check-out date must be after check-in date. Please rebook again");
+
+                exe.pressEnterToContinue();
+                exe.clearConsole();
+            }else{
+                Hotel chosenHotel = HotelLists.listOfHotels().get(index-1);
+                ArrayList<Room> roomsOfChosenHotel = chosenHotel.viewRooms();
+                ArrayList<Reservation> reservations = chosenHotel.viewReservations();
+
+                System.out.println(chosenHotel.getHotelName()+"'s available rooms on your specified dates\n");
+        
+                roomAvailable(roomsOfChosenHotel, reservations, checkOut, checkIn);
+
+                System.out.print("*------------------------------------*\n\n");
+                
+                System.out.println("Enter room index: ");
+                int roomIndex = sc.nextInt();
+                sc.nextLine();
+
+                System.out.println("Enter customer name: ");
+                String name = sc.nextLine();
+
+                System.out.println();
+
+                Room chosenRoom = roomsOfChosenHotel.get(roomIndex-1);
+
+                Reservation reserved = new Reservation(name, checkIn, checkOut, chosenRoom);
+                chosenHotel.viewReservations().add(reserved);
+
+                exe.clearConsole();
+
+                System.out.println("\nThank you for choosing " + chosenHotel.getHotelName() + ", your reservation was succesfully booked!");
+                System.out.println("*-------------------------------*");
+                
+                System.out.println(reserved.displayConfirmation(reserved));
+
+                System.out.println();
+
+                exe.pressEnterToContinue();
+                exe.clearConsole();
+            }
+        }
+    }
+
+    private void roomAvailable(ArrayList<Room> roomsOfChosenHotel, ArrayList<Reservation> reservations, Date checkOut, Date checkIn){
+        for (Room room : roomsOfChosenHotel) {
+            boolean isAvailable = true; 
+
+            for (Reservation reservation : reservations) {
+                if (room.equals(reservation.room())) { 
+                    if (!(checkOut.before(reservation.checkInDate()) || checkIn.after(reservation.checkOutDate()))) {
+                        isAvailable = false;
+                    }
+                }
+            }
+
+            if (isAvailable) {
+                System.out.println(room.getRoomName()); 
+            }
+        }
+
+    }
+
+    private boolean verifyMonth(String month){
+        ArrayList<String> Month = new ArrayList<>(Arrays.asList("January", "February", "March",
+                                                                      "April", "May", "June", "July",
+                                                                      "August", "September", "October",
+                                                                      "November", "December"));
+
+        for (int i = 0; i < Month.size(); i++)
+            if (month.equalsIgnoreCase(Month.get(i))){
+                return true;
+            }
+        
+        return false;
+    }
+
+}
