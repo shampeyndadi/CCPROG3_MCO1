4c6aab83a67d2d765d29a9ee3a12c3d158e96dd0
Jedidiah <152191672+shampeyndadi@users.noreply.github.com>
2024-06-25 03:50:02 +0800

    Add files via upload

---
+++ MCO1_Hotel/src/Execute.java
@@ -0,0 +1,194 @@
+import java.util.Scanner;
+public class Execute{
+    private Hotels HRS;
+    private ManageHotel HotelManager;
+    private SimulateBooking BookHotel;
+    private Scanner sc;
+    private Interface layout;
+    private ViewHotel hotelFinder;
+    
+
+    public Execute(){
+        this.HRS = new Hotels();
+        this.HotelManager = new ManageHotel(HRS);
+        this.BookHotel = new SimulateBooking(HRS);
+        this.sc = new Scanner(System.in);
+        this.layout = new Interface();
+        this.hotelFinder = new ViewHotel(HRS);
+    }
+
+    public void clearConsole(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (Exception e) {
+            for (int i = 0; i < 50; ++i) 
+                System.out.println();
+        } 
+    }
+
+    public void pressEnterToContinue()
+    { 
+           System.out.println("Press Enter key to continue...");
+           try
+           {
+                System.in.read();
+           }  
+           catch(Exception e)
+           {}  
+    }
+
+    private void ManageHotel(){
+        boolean run = true;
+        int option;
+    
+        do{
+            layout.ManageHotel();
+            System.out.print("\n          [Enter option]: ");
+            option = sc.nextInt();
+            switch(option){
+                case 1:
+                    clearConsole();
+                    HotelManager.AddRooms();
+                    pressEnterToContinue();
+                    clearConsole();
+                    break;
+                case 2:
+                    clearConsole();
+                    HotelManager.changeHotelName();
+                    pressEnterToContinue();
+                    clearConsole();
+                    break;
+                case 3:
+                    clearConsole();
+                    HotelManager.RemoveRooms();
+                    pressEnterToContinue();
+                    clearConsole();
+                    break;
+                case 4:
+                    clearConsole();
+                    HotelManager.changeBasePrice();
+                    pressEnterToContinue();
+                    clearConsole();
+                    break;
+                case 5:
+                    clearConsole();
+                    HotelManager.removeReservation();
+                    pressEnterToContinue();
+                    clearConsole();
+                    break;
+                case 6:
+                    clearConsole();
+                    HotelManager.removeHotel();
+                    pressEnterToContinue();
+                    clearConsole();
+                case 7: 
+                    clearConsole();
+                    run = false;
+                    clearConsole();
+                    break; 
+            }
+        } while (run != false);
+    }
+
+    private void ViewHotel(){
+        boolean run = true;
+        int option;
+    
+        do{
+            layout.ViewHotel();
+            System.out.print("\n          [Enter option]: ");
+            option = sc.nextInt();
+            switch(option){
+                case 1:
+                    clearConsole();
+                    hotelFinder.displayHighInformation();
+                    pressEnterToContinue();
+                    clearConsole();
+                    break;
+                case 2:
+                    clearConsole();
+                    SpecificInformation();
+                    break;
+                case 3: 
+                    clearConsole();
+                    run = false;
+                    clearConsole();
+                    break; 
+            }
+        } while (run != false);
+    }
+
+    private void SpecificInformation(){
+        boolean run = true;
+        int option;
+    
+        do{
+            layout.SpecificInformation();
+            System.out.print("\n          [Enter option]: ");
+            option = sc.nextInt();
+            switch(option){
+                case 1:
+                    clearConsole();
+                    hotelFinder.displayHotelInformation();
+                    break;
+                case 2:
+                    clearConsole();
+                    hotelFinder.displayRoomInformation();
+                    break;
+                case 3:
+                    clearConsole();
+                    hotelFinder.displayReservationInformation();
+                    break;
+                case 4: 
+                    clearConsole();
+                    run = false;
+                    clearConsole();
+                    break; 
+            }
+        } while (run != false);
+    }
+
+    public void execute(){
+        boolean run = true;
+        int option;
+        
+        do{
+            layout.MainMenu();
+            System.out.print("\n          [Enter option]: ");
+            option = sc.nextInt();
+            switch(option){
+                case 1:
+                    clearConsole();
+                    HRS.createHotel();
+                    pressEnterToContinue();
+                    clearConsole();
+                    break;
+                case 2:
+                    clearConsole();
+                    ManageHotel();
+                    break;
+                case 3: 
+                    clearConsole();
+                    BookHotel.bookHotel();
+                    break;
+                case 4: 
+                    clearConsole();
+                    ViewHotel();
+                    break;
+                case 5: 
+                    run = false;
+                    clearConsole();
+                    System.out.println("[Program End]");
+                    break; 
+            }
+        } while (run != false);
+        
+    }
+}   
---
+++ MCO1_Hotel/src/Reservation.java
@@ -0,0 +1,59 @@
+/* 
+   
+ */
+
+import java.util.ArrayList;
+
+class Reservation {
+
+    private String guestName;
+    private Date  checkInDate;
+    private Date checkOutDate;
+    private double totalPrice;
+    private Room room;
+    private ArrayList<Room> listOfRooms;
+
+    public Reservation(String guestName, Date checkInDate, Date checkOutDate, Room room) {
+        this.guestName = guestName;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.totalPrice = (checkOutDate.getDay() - checkInDate.getDay()) * room.getRoomPrice();
+        this.room = room;
+    }
+
+    public double TotalPrice() {
+        return totalPrice; // to be fixed
+    }
+
+    public String guestName(){
+        return guestName; // customer name 
+    }
+
+    public Date checkInDate(){
+        return checkInDate; // check in date 
+    }
+
+    public Date checkOutDate(){
+        return checkOutDate; // check out date 
+    }
+
+    public ArrayList<Room> rooms(){
+        return listOfRooms; // list of rooms 
+    }
+
+    public Room room(){
+        return room; // what room customer books
+    }
+
+    public String displayConfirmation(Reservation reserved){
+        return "Customer name: " + reserved.guestName()
+            + "\nSuite: " + reserved.room().getRoomName()
+            + "\nCheck in: " + reserved.checkInDate.getDayWithSuffix() + " of the month" 
+            + "\nExpected check out: " + reserved.checkOutDate.getDayWithSuffix() + " of the month" 
+            + "\n*-----------------------------------*\n"
+            + "Total price: " + reserved.TotalPrice();
+
+    }
+
+
+}
